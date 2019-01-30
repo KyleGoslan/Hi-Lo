8bd5c6ab8db41c4a58023899d87d6d3b1d0d3feb
Kyle Goslan <KyleGoslan@users.noreply.github.com>
2019-01-30 14:51:46 +0000

    Initial commit

--- Hi-Lo/ViewController.swift
+++ Hi-Lo/ViewController.swift
@@ -1,20 +1,59 @@
-//
-//  ViewController.swift
-//  Hi-Lo
-//
-//  Created by Kyle Goslan on 1/30/19.
-//  Copyright © 2019 Kyle Goslan. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  
+  var score = 0
+  var target = 0
+  var newTarget = 0
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    reset()
+  }
+  
+  
+  @IBAction func guessHigher(_ sender: Any) {
+    newTarget = Int.random(in: 0...100)
+    
+    if newTarget > target {
+      correct()
+    } else {
+      wrong()
+    }
+  }
+  
+  @IBAction func guessLower(_ sender: Any) {
+    newTarget = Int.random(in: 0...100)
+    
+    if newTarget < target {
+      correct()
+    } else {
+      wrong()
+    }
   }
 
+  func reset() {
+    score = 0
+    target = 0
+    newTarget = 0
+    targetLabel.text = "\(target)"
+    scoreLabel.text = "\(score)"
+  }
+  
+  
+  func correct() {
+    score += 1
+    scoreLabel.text = "\(score)"
+    target = newTarget
+    targetLabel.text = "\(target)"
+  }
+  
+  func wrong() {
+    reset()
+  }
 
 }
 
